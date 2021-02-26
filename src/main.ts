9a64dd183f76c8766ef84714e0f2b5301d8d8f43
carwack <sybren.willemot@gmail.com>
2021-02-26 23:13:48 +0100

    feat(✨): add chakra ui vue next package and chakra factory

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ChakraUIVuePlugin, { chakra } from '@chakra-ui/vue-next'
+import { domElements } from '@chakra-ui/vue-system'
 
-createApp(App).mount('#app')
+const app = createApp(App).use(ChakraUIVuePlugin)
+
+domElements.forEach((tag) => {
+  app.component(`chakra.${tag}`, chakra(tag))
+})
+
+app.mount('#app')
