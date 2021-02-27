5c1ccde0635dfa8c27e7e1b257cb06de32cecc29
carwack <sybren.willemot@gmail.com>
2021-02-27 02:22:41 +0100

    feat(📦): add feather icons package

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import ChakraUIVuePlugin, { chakra } from '@chakra-ui/vue-next'
 import { domElements } from '@chakra-ui/vue-system'
+import { feGithub } from 'feather-icons-paths'
 
-const app = createApp(App).use(ChakraUIVuePlugin)
+const app = createApp(App).use(ChakraUIVuePlugin, {
+  icons: {
+    library: {
+      feGithub
+    }
+  }
+})
 
 domElements.forEach((tag) => {
   app.component(`chakra.${tag}`, chakra(tag))
