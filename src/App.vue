53ceb3eb7eb7cc5a9c798de09716e7f97660a7cf
carwack <sybren.willemot@gmail.com>
2021-02-27 01:10:26 +0100

    feat(💄): update welcome content

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <chakra.div>hello chakra</chakra.div>
+  <chakra.img m="auto" h="150px" alt="Chakra UI Vue logo" src="/src/assets/chakra-logo.jpg" />
+  <chakra.header
+    pos="relative"
+  >
+    <chakra.div
+      d="flex"
+      :px="[8, 8]"
+      :py="10"
+      :h="['auto', 'auto', '60vh']"
+      align-items="center"
+      justify-content="center"
+    >
+      <chakra.div
+        d="flex"
+        flex-dir="column"
+        :align-items="['flex-start', 'center']"
+        justify-content="center"
+        :text-align="['left', 'center']"
+        w="550px"
+      >
+        <chakra.h1 font-size="2em" font-weight="bold">
+          Chakra UI Vue Next - Build Accessible Vue (3) apps with Speed ⚡️
+        </chakra.h1>
+        <chakra.p mt="4" mb="6">
+          Chakra UI Vue Next is a simple modular and accessible component library that gives you the building blocks to build Vue applications with speed.
+        </chakra.p>
+        <chakra.div
+          d="flex"
+          flex-dir="row"
+        >
+          <CButton
+            as="a"
+            text-decoration="none"
+            href="https://github.com/chakra-ui/chakra-ui-vue-next"
+            left-icon="star"
+            size="lg"
+          >
+            Get started
+          </CButton>
+          <CButton
+            as="a"
+            text-decoration="none"
+            href="https://github.com/chakra-ui/chakra-ui-vue-next"
+            left-icon="github"
+            ml="4"
+            variant="outline"
+            size="lg"
+          >
+            Github
+          </CButton>
+        </chakra.div>
+      </chakra.div>
+    </chakra.div>
+  </chakra.header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { CButton } from "@chakra-ui/vue-next"
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    CButton
   }
 })
 </script>
